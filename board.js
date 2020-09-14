a377287614d02b10cbcd0a97c682f9198bc5e16f
Roy <41695919+roygb1v@users.noreply.github.com>
2020-09-14 23:05:13 +0800

    Update board.js

--- board.js
+++ board.js
@@ -1,48 +1,47 @@
-const checkWord = (board, word) => {
-  let k = 0;
-  for (let i = 0; i < board.length; i += 1) {
-    for (let j = 0; j < board[0].length; j += 1) {
-      let cell = board[i][j];
-      let char = word[k];
-      if (cell === char) {
-        let found = dfs(board, i, j, k, word)
-        if (found) {
-          return true;
-        }
+const exist = (board, word) => {
+  let rows = board.length;
+  let cols = board[0].length;
+  for (let i = 0; i < rows; i += 1) {
+    for (let j = 0; j < cols; j += 1) {
+      if (backtrack(board, i, j, word)) {
+        return true;
       }
     }
   }
   return false;
 }
 
-const dfs = (board, i, j, index, word) => {
-  if (word.length === index) return true;
-  if (i >= 0 && j >= 0 && i < board.length && j < board[0].length) {
-    let cell = board[i][j];
-    let char = word[index];
-    if (cell === char) {
-      board[i][j] = '#';
-      for (let x = -1; x < 2; x += 1) {
-        for (let y = -1; y < 2; y += 1) {
-          let ok = dfs(board, i + x, j + y, index + 1, word);
-          if (ok) {
-            return true;
-          }
-        }
-      }
-      return false;
+const backtrack = (board, row, col, suffix) => {
+  if (suffix.length === 0) {
+    return true;
+  }
+  let rows = board.length;
+  let cols = board[0].length;
+  if (row < 0 || row === rows || col < 0 || col === cols || board[row][col] !== suffix[0]) {
+    return false;
+  }
+  board[row][col] = '#';
+  let directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+  for (let idx in directions) {
+    let [rowOffset, colOffset] = directions[idx]
+    if (backtrack(board, row + rowOffset, col + colOffset, suffix.slice(1))) {
+      return true;
     }
   }
+  board[row][col] = suffix[0];
+  return false;
 }
 
 const board = [
-  ['E', 'B', 'Y', 'S'],
-  ['L', 'D', 'R', 'A'],
-  ['R', 'A', 'D', 'T'],
-  ['C', 'W', 'H', 'I']
+  ['A','B','C','E'],
+  ['S','F','C','S'],
+  ['A','D','E','E']
 ]
 
-console.log(checkWord(board, 'CARBY'));
-// console.log(checkWord(board, 'CARP'));
-// console.log(checkWord(board, 'BRAT'));
-console.log('board', board)
+let word1 = 'ABCCED';
+let word2 = 'SEE';
+let word3 = 'ABCB';
+
+console.log(exist(board, word1));
+console.log(exist(board, word2));
+console.log(exist(board, word3));
